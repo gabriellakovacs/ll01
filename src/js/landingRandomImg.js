c2fdc2faff7c004562417327ce50f3b9a74a0f3f
gabriella kovacs <kovacsxgabriella@gmail.com>
2017-11-17 13:31:25 +0100

    we do this with twig

--- src/js/landingRandomImg.js
+++ src/js/landingRandomImg.js
@@ -1,8 +1,8 @@
-var numberOfImgs = 10;
-var imgList = [];
-var img = document.querySelector('.js-landing-img');
-
-function chooseRandomImg() {
-  var imgIndex = Math.rand() * numberOfImgs;
-
-}
+// var numberOfImgs = 10;
+// var imgList = [];
+// var img = document.querySelector('.js-landing-img');
+//
+// function chooseRandomImg() {
+//   var imgIndex = Math.rand() * numberOfImgs;
+//
+// }
