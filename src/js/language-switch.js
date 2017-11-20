cb8f3d92fa7c11167625cbf46325f42503581c7e
gabriella kovacs <kovacsxgabriella@gmail.com>
2017-11-20 22:54:23 +0100

    add language switch

---
+++ src/js/language-switch.js
@@ -0,0 +1,43 @@
+(function () {
+
+    var languageSwitchEn = $('.js-language-switch-en');
+    var languageSwitchDe = $('.js-language-switch-de');
+    //var languageSwitchEn = $('.js-language-switch-en');
+    var body = $('body');
+
+
+    languageSwitchEn.bind( "click", function() {
+        body.removeClass('language--de').addClass('language--en');
+        closeMenu();
+    });
+
+
+    languageSwitchDe.bind( "click", function() {
+        body.removeClass('language--en').addClass('language--de');
+        closeMenu();
+    });
+
+
+    function closeMenu() {
+      menuCloseButton.classList.add('rotate');
+
+      setTimeout(function(){
+        menu.classList.remove('open');
+        // html.style.overflow = 'auto';
+        // body.style.overflow = 'auto';
+      }, 350);
+
+      setTimeout(function(){
+        menuCloseButton.classList.remove('rotate');
+      }, 1500);
+
+    }
+
+    // function switchToEnglish() {
+    //     body.removeClass('').addClass('');
+    // }
+    //
+    // function switchToGerman() {
+    //     body.removeClass('').addClass('');
+    // }
+}());
