31e2fcde57c2718c1e2310dcdd571a82e4b7de31
gabriella kovacs <kovacsxgabriella@gmail.com>
2017-11-22 22:14:44 +0100

    to prev

---
+++ src/js/landingDubbleHover.js
@@ -0,0 +1,20 @@
+var $landingImg = $('.js-landing-img');
+
+$('.js-scrollNext').mousemove(function (event) {
+
+    if (document.elementFromPoint) {
+        var $scrollNext = $(this);
+
+        // hide scrollNext
+        $scrollNext.css('visibility', 'hidden');
+        // get the element underneath, if any
+        var $under = $(document.elementFromPoint(event.clientX, event.clientY));
+        // show again scrollNext
+        $scrollNext.css('visibility', 'visible');
+        $under.hasClass('js-landing-img') ?
+           $landingImg.addClass('hover') :
+           $landingImg.removeClass('hover');
+    }
+}).mouseleave(function () {
+    $landingImg.removeClass('hover');
+});
