9aec61bba4d04852ad0118153a54779b278dc8cb
gabriella kovacs <kovacsxgabriella@gmail.com>
2017-11-23 13:13:55 +0100

    remove white menu button and set galleries to beginning when scrolling away forma section

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,30 +1,42 @@
-var infoHasFired = false;
-
 $(function() {
-  $.scrollify({
-    section : "section",
-    setHeights: false,
-    overflowScroll: false,
-    scrollbars: false,
-    after:function(index, sections) {
-        if(!infoHasFired && index === 1) {
-            sections[1].addClass('in-view');
-            infoHasFired = true;
+
+    var infoHasFired = false;
+    var menuButton = $('.js-open-menu');
+    var galleryItemList = $('.js-galleryItem');//active
+    var galleryStarter = $('.js-start-gallery'); //move-right
+    var galleryNavNext = $('.js-galleryNext');
+    var galleryNavPrev = $('.js-galleryPrev');
+
+    $.scrollify({
+        section : "section",
+        setHeights: false,
+        overflowScroll: false,
+        scrollbars: false,
+        after:function(index, sections) {
+            if(!infoHasFired && index === 1) {
+                sections[1].addClass('in-view');
+                infoHasFired = true;
+            }
+        },
+        before: function(){
+            menuButton.removeClass('white');
+            galleryItemList.removeClass('active');
+            galleryStarter.removeClass('move-right');
+            galleryNavNext.removeClass('active');
+            galleryNavPrev.removeClass('active');
         }
-    }
-  });
-});
+    });
+
+    $('.js-scrollNext').click(function() {
+        $.scrollify.next();
+    });
 
 
-$('.js-scrollNext').click(function() {
-    $.scrollify.next();
-});
-
-
-$( window ).resize(function() {
-  if($(window).width() < $(window).height()) {
-      $.scrollify.disable();
-  } else if($.scrollify.isDisabled()) {
-      $.scrollify.enable();
-  }
+    $( window ).resize(function() {
+      if($(window).width() < $(window).height()) {
+          $.scrollify.disable();
+      } else if($.scrollify.isDisabled()) {
+          $.scrollify.enable();
+      }
+    });
 });
