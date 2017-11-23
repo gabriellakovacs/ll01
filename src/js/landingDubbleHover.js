88389c81bfd2596d284886280d53af154fd6a3ff
gabriella kovacs <kovacsxgabriella@gmail.com>
2017-11-23 13:16:57 +0100

    wrap functions: no namespace solution

--- src/js/landingDubbleHover.js
+++ src/js/landingDubbleHover.js
@@ -1,20 +1,22 @@
-var $landingImg = $('.js-landing-img');
+$(function() {
+    var $landingImg = $('.js-landing-img');
 
-$('.js-scrollNext').mousemove(function (event) {
+    $('.js-scrollNext').mousemove(function (event) {
 
-    if (document.elementFromPoint) {
-        var $scrollNext = $(this);
+        if (document.elementFromPoint) {
+            var $scrollNext = $(this);
 
-        // hide scrollNext
-        $scrollNext.css('visibility', 'hidden');
-        // get the element underneath, if any
-        var $under = $(document.elementFromPoint(event.clientX, event.clientY));
-        // show again scrollNext
-        $scrollNext.css('visibility', 'visible');
-        $under.hasClass('js-landing-img') ?
-           $landingImg.addClass('hover') :
-           $landingImg.removeClass('hover');
-    }
-}).mouseleave(function () {
-    $landingImg.removeClass('hover');
-});
+            // hide scrollNext
+            $scrollNext.css('visibility', 'hidden');
+            // get the element underneath, if any
+            var $under = $(document.elementFromPoint(event.clientX, event.clientY));
+            // show again scrollNext
+            $scrollNext.css('visibility', 'visible');
+            $under.hasClass('js-landing-img') ?
+               $landingImg.addClass('hover') :
+               $landingImg.removeClass('hover');
+        }
+    }).mouseleave(function () {
+        $landingImg.removeClass('hover');
+    });
+})
