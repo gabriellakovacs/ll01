4f892b24b5aa6d6b4cef64fb6530666d96778bdf
gabriella kovacs <kovacsxgabriella@gmail.com>
2018-01-05 19:14:46 +0100

    remove unused js

--- src/js/landingRandomImg.js
+++
@@ -1,8 +0,0 @@
-// var numberOfImgs = 10;
-// var imgList = [];
-// var img = document.querySelector('.js-landing-img');
-//
-// function chooseRandomImg() {
-//   var imgIndex = Math.rand() * numberOfImgs;
-//
-// }
