354c40c43f54fc656c5d8671748a95825a136284
gabriella kovacs <kovacsxgabriella@gmail.com>
2018-01-06 01:31:18 +0100

    start languege switch afteer  domcontentloaded

--- src/js/language-switch.js
+++ src/js/language-switch.js
@@ -1,43 +1,36 @@
-(function () {
+document.addEventListener("DOMContentLoaded", function() {
 
-    var languageSwitchEn = $('.js-language-switch-en');
-    var languageSwitchDe = $('.js-language-switch-de');
-    //var languageSwitchEn = $('.js-language-switch-en');
-    var body = $('body');
+        var languageSwitchEn = $('.js-language-switch-en');
+        var languageSwitchDe = $('.js-language-switch-de');
+        //var languageSwitchEn = $('.js-language-switch-en');
+        var body = $('body');
 
 
-    languageSwitchEn.bind( "click", function() {
-        body.removeClass('language--de').addClass('language--en');
-        closeMenu();
+        languageSwitchEn.bind( "click", function() {
+            body.removeClass('language--de').addClass('language--en');
+            closeMenu();
+        });
+
+
+        languageSwitchDe.bind( "click", function() {
+            body.removeClass('language--en').addClass('language--de');
+            closeMenu();
+        });
+
+
+        function closeMenu() {
+          menuCloseButton.classList.add('rotate');
+
+          setTimeout(function(){
+            menu.classList.remove('open');
+            // html.style.overflow = 'auto';
+            // body.style.overflow = 'auto';
+          }, 350);
+
+          setTimeout(function(){
+            menuCloseButton.classList.remove('rotate');
+          }, 1500);
+
+        }
+
     });
-
-
-    languageSwitchDe.bind( "click", function() {
-        body.removeClass('language--en').addClass('language--de');
-        closeMenu();
-    });
-
-
-    function closeMenu() {
-      menuCloseButton.classList.add('rotate');
-
-      setTimeout(function(){
-        menu.classList.remove('open');
-        // html.style.overflow = 'auto';
-        // body.style.overflow = 'auto';
-      }, 350);
-
-      setTimeout(function(){
-        menuCloseButton.classList.remove('rotate');
-      }, 1500);
-
-    }
-
-    // function switchToEnglish() {
-    //     body.removeClass('').addClass('');
-    // }
-    //
-    // function switchToGerman() {
-    //     body.removeClass('').addClass('');
-    // }
-}());
